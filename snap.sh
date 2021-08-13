ff2abea11aff100e4254bdc0ba597be50d18cf62
Loïs Civiero <lois.civiero@gmail.com>
2021-08-13 12:56:16 +0200

    Update snap.sh
    Updated url to use latest snapshot

--- snap.sh
+++ snap.sh
@@ -2,7 +2,7 @@
 
 rm /wax/snapshots/*.*
 rm -rf /wax/data/*.*
-curl -o /wax/snapshots/snapshot.bin.tar.gz https://snapshots.waxsweden.org/snapshots/57abfa/2.0/snapshot-125583653.bin.tar.gz
+curl -o /wax/snapshots/snapshot.bin.tar.gz https://snapshots.waxsweden.org/snapshots/57abfa/2.0/latest
 tar -zxf /wax/snapshots/snapshot.bin.tar.gz -C /wax/snapshots/
 rm /wax/snapshots/*.tar.gz
 mv /wax/snapshots/*.bin /wax/snapshots/snapshot.bin
